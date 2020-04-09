1f5d2fee39244bf3bb8bb7818a7a8564b83d920d
Gareth1980 <garethbuckley@hotmail.com>
2020-04-09 17:03:14 +0000

    on index.html, I updated headers, started about donegal town section with read more button using javascript, started things to do section with tabs again using javascript, updated contact us section with 2 resonsive columns. Also added you tube icon to footer.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,1 +1,31 @@
-console.log("Hello World");
+/* About-heading read more/less button function */
+function myFunction() {
+  var dots = document.getElementById("dots");
+  var moreText = document.getElementById("more");
+  var btnText = document.getElementById("myBtn");
+
+  if (dots.style.display === "none") {
+    dots.style.display = "inline";
+    btnText.innerHTML = "Read more";
+    moreText.style.display = "none";
+  } else {
+    dots.style.display = "none";
+    btnText.innerHTML = "Read less";
+    moreText.style.display = "inline";
+  }
+}
+
+/* Things To Do Tabs function */
+function openThingsToDo(evt, activity) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(activity).style.display = "block";
+  evt.currentTarget.className += " active";
+}
