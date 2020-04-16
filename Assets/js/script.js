88b4e363a260d06c778fd929f777e1cdbf90ced3
Gareth1980 <garethbuckley@hotmail.com>
2020-04-16 14:02:29 +0000

    updated about section, inserted scroll to top button, updated contact us section and updated styling for scroll to top button and contact us button.

--- Assets/js/script.js
+++ Assets/js/script.js
@@ -1,3 +1,19 @@
+/* Scroll to top button  */
+var btn = $('#button');
+
+$(window).scroll(function() {
+  if ($(window).scrollTop() > 300) {
+    btn.addClass('show');
+  } else {
+    btn.removeClass('show');
+  }
+});
+
+btn.on('click', function(e) {
+  e.preventDefault();
+  $('html, body').animate({scrollTop:0}, '300');
+});
+
 /* About-heading read more/less button function */
 function myFunction() {
   var dots = document.getElementById("dots");
