9b9b1347a8e8446c4ee1542702319126b7a2c59a
Gareth1980 <garethbuckley@hotmail.com>
2020-04-20 21:16:56 +0000

    amended map.js, trying to add info windows to markers, added information for markers A, B and C

--- Assets/js/maps.js
+++ Assets/js/maps.js
@@ -1,5 +1,3 @@
-console.log("Hello");
-
 function initMap() {
 
   var map = new google.maps.Map(document.getElementById('map'), {
@@ -19,6 +17,22 @@ function initMap() {
       position: location,
       label: labels[i % labels.length]
     });
+
+google.maps.event.addListener(map, 'click', function(event) {
+  placeMarker(map, event.latLng);
+});
+
+function placeMarker(map, location) {
+  var marker = new google.maps.Marker({
+    position: location,
+    map: map
+  });
+  var infowindow = new google.maps.InfoWindow({
+    content: content[i % labels.length]
+  });
+  infowindow.open(map,marker);
+}
+
   });
 
   // Add a marker clusterer to manage the markers.
@@ -26,9 +40,15 @@ function initMap() {
       {imagePath: 'https://developers.google.com/maps/documentation/javascript/examples/markerclusterer/m'});
 }
 var locations = [
-  {lat: 54.688242, lng: -8.066898},
-  {lat: 54.656105, lng: -8.114178},
-  {lat: 54.653878, lng: -8.110944},
+  {content: '<strong>The Lough Eske Castle</strong><br>\
+                <a href="https://goo.gl/maps/C26BBK3hQ3FAnhT17">Get Directions</a>',
+            lat: 54.688242, lng: -8.066898},
+  {content: '<strong>The Gateway Lodge</strong><br>\
+                <a href="https://goo.gl/maps/H2oLX9hmd89z5rBb7">Get Directions</a>',
+            lat: 54.656105, lng: -8.114178},
+  {content: '<strong>The Abbey Hotel</strong><br>\
+                <a href="https://goo.gl/maps/ytKooFj8a8niQn78A">Get Directions</a>', 
+            lat: 54.653878, lng: -8.110944},
   {lat: 54.656578, lng: -8.125806},
   {lat: 54.660085, lng: -8.111458},
   {lat: 54.656971, lng: -8.108012},
@@ -47,3 +67,62 @@ var locations = [
   {lat: 54.654792, lng: -8.111367},
   {lat: 54.652823, lng: -8.111043}
 ]
+
+/*
+function initMap() {
+  var lougheske = {
+    info:
+      '<strong>The Lough Eske Castle</strong><br>\
+    	<a href="https://goo.gl/maps/C26BBK3hQ3FAnhT17">Get Directions</a>',
+        lat: 54.688242, lng: -8.066898,
+  }
+
+  var gatewaylodge = {
+    info:
+      '<strong>The Gateway Lodge</strong><br>\
+    	<a href="https://goo.gl/maps/H2oLX9hmd89z5rBb7">Get Directions</a>',
+        lat: 54.656105, lng: -8.114178,
+  }
+
+  var abbeyhotel = {
+    info:
+      '<strong>The Abbey Hotel</strong><br>\
+    	<a href="https://goo.gl/maps/ytKooFj8a8niQn78A">Get Directions</a>',
+    lat: 54.653878, lng: -8.110944,
+  }
+
+  var locations = [
+    [lougheske.info, lougheske.lat, lougheske.long, 0],
+    [gatewaylodge.info, gatewaylodge.lat, gatewaylodge.long, 1],
+    [abbeyhotel.info, abbeyhotel.lat, abbeyhotel.long, 2],
+  ]
+
+  var map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 15,
+    center: {lat: 54.653835, lng: -8.110328},
+    mapTypeId: google.maps.MapTypeId.ROADMAP,
+  })
+
+  var infowindow = new google.maps.InfoWindow({})
+
+  var marker, i
+
+  for (i = 0; i < locations.length; i++) {
+    marker = new google.maps.Marker({
+      position: new google.maps.LatLng(locations[i][1], locations[i][2]),
+      map: map,
+    })
+
+    google.maps.event.addListener(
+      marker,
+      'click',
+      (function(marker, i) {
+        return function() {
+          infowindow.setContent(locations[i][0])
+          infowindow.open(map, marker)
+        }
+      })(marker, i)
+    )
+  }
+}
+*/
